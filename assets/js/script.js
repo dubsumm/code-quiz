c25778e46f3ada5a3ed8052cd8d580b480d527d7
dubsumm <w.b.summerlin@gmail.com>
2022-06-26 22:07:01 -0400

    intial commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//maybe implement the revolving array as the questions display similar 
+//to the images activity
+
+//display main page with instructions and a link to start the quiz
+//when start button is pushed display first question and start the timer
